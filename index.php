cd2afff0edc16bc3fcc946b4afad5430743dbe4f
LucasMaroy <lucasmaroy42@gmail.com>
2024-08-16 14:58:16 +0200

    Ajout hover hero/Nouveau format poster -> page detail+point entre info film

--- index.php
+++ index.php
@@ -31,6 +31,54 @@ try {
 }
 
 ?>
+<?php
+//images pour le hero
+$elements = [
+    [
+        'image' => './image/hero_image/Chernobyles.jpg',
+        'title' => 'Chernobyl',
+        'imdb_vote' => '9.3',
+        'release_date' => '2019'
+    ],
+    [
+      'image' => './image/hero_image/BohemianRhapsody-Crop.png',
+      'title' => 'Bohemian Rhapsody',
+      'imdb_vote' => '7.9',
+      'release_date' => '2019'
+    ],
+    [
+      'image' => './image/hero_image/SISU.jpg',
+      'title' => "Sisu",
+      'imdb_vote' => '6.9',
+      'release_date' => '2022'
+    ],
+    [
+      'image' => './image/hero_image/NightAgent-Crop.png',
+      'title' => "The Night Agent",
+      'imdb_vote' => '7.5',
+      'release_date' => '2023'
+    ],
+    [
+      'image' => './image/hero_image/DropOfGod-Crop.png',
+      'title' => "Drop Of God",
+      'imdb_vote' => '8.0',
+      'release_date' => '2023'
+    ],
+    [
+      'image' => './image/hero_image/Avatar_2-Crop.png',
+      'title' => "Avatar: The Way of Water",
+      'imdb_vote' => '7.5',
+      'release_date' => '2022'
+    ],
+    [
+      'image' => './image/hero_image/Lucky.png',
+      'title' => "Loki",
+      'imdb_vote' => '8.2',
+      'release_date' => '2021-2023'
+    ],
+];
+$n = count($elements);
+?>
 
 <!DOCTYPE html>
 <html lang="en" class="h-screen w-screen">
@@ -46,15 +94,28 @@ try {
 <body class="bg-halfBlack sm:w-screen w-full h-fit">
   <?php include_once("./header.php"); ?>
   <main class="w-[80%] sm:container mx-auto h-fit">
-    <section class="cate-main w-full h-[748px] sm:h-[801px] flex items-center sm:my-[39px] overflow-x-scroll">
-      <article class="w-full h-[622px] sm:h-[691px] flex justify-between space-x-3">
-        <img src="image/Chernobyles.jpg" alt="Chernobyles" name="img-hero_01" class="object-cover h-[622px] sm:h-[652px] w-[93.16px] sm:w-[190px] rounded-xl">
-        <img src="image/BohemianRhapsody-Crop.png" alt="Bohemian-Rhapsody" name="img-hero_02" class="object-cover h-[622px] sm:h-[652px] w-[93.16px] sm:w-[190px] rounded-xl">
-        <img src="image/SISU.jpg" alt="SISU" name="img-hero_03" class="object-cover h-[622px] sm:h-[652px] w-[93.16px] sm:w-[190px] rounded-xl">
-        <img src="image/NightAgent-Crop.png" alt="Night_Agent" name="img-hero_04" class="object-cover h-[622px] sm:h-[652px] w-[93.16px] sm:w-[190px] rounded-xl">
-        <img src="image/DropOfGod-Crop.png" alt="DropOfGod" name="img-hero_05" class="object-cover h-[622px] sm:h-[652px] w-[93.16px] sm:w-[190px] rounded-xl">
-        <img src="image/Avatar_2-Crop.png" alt="Avatar_2" name="img-hero_06" class="object-cover h-[622px] sm:h-[652px] w-[93.16px] sm:w-[190px] rounded-xl">
-        <img src="image/Lucky.png" alt="Lucky" name="img-hero_07" class="object-cover h-[622px] sm:h-[652px] w-[93.16px] sm:w-[190px] rounded-xl">
+    <section class="cate-main h-[748px] sm:h-[801px] flex items-center sm:my-[39px]">
+      <article class="hero-article h-[622px] sm:h-[691px] flex *:mr-6 last:*:mr-0 overflow-x-auto">
+        <?php for ($i = 0; $i < $n; $i++): ?>
+              <?php 
+              $element = $elements[$i % count($elements)];
+              ?>
+              <article class="card1 flex-shrink-0 group relative h-[622px] sm:h-[652px] w-[93.16px] sm:w-[190px] overflow-hidden sm:hover:w-[485.21px] hover:w-[462.88px]">
+                  <img src="<?php echo $element['image']; ?>" alt="<?php echo $element['title']; ?> Test" class="transition-transform duration-500 ease-in-out w-full h-full object-cover rounded-xl">
+                  <section class="absolute flex flex-col justify-between inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out p-4">
+                      <h2 class="text-white text-4xl font-[570] uppercase leading-none h-5"><?php echo $element['title']; ?></h2>
+                      <article class="flex justify-between items-center">
+                          <button class="flex items-center h-fit">
+                              <img class="mr-1.5 h-8 w-8" src="image/grade_32dp_FFF_FILL1_wght400_GRAD0_opsz40.svg" alt="IMDB Rating">
+                              <span class="text-white text-lg leading-none"><?php echo $element['imdb_vote']; ?></span>
+                          </button>
+                          <button class="flex items-center h-fit">
+                              <span class="text-white text-lg leading-none"><?php echo $element['release_date']; ?></span>
+                          </button>
+                      </article>
+                  </section>
+              </article>
+          <?php endfor; ?>
       </article>
     </section>
     <section class="cate-main w-full h-fit overflow-x-scroll mb-14">
